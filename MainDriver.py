7e85b3c7eb18083801444a34ae4837b4cfb27707
sesan07 <31746240+sesan07@users.noreply.github.com>
2020-03-11 14:36:34 -0300

    Removed all tabs from MainWindow.py

--- MainDriver.py
+++ MainDriver.py
@@ -35,24 +35,24 @@ if __name__ == "__main__":
     main_window_ui = ui.MainWindow.Ui_mainWindow()
     main_window_ui.setupUi(main_window)
 
-    # region Setup Tab Controllers
-    manage_vendors_controller = ManageVendorsController(main_window_ui)
-    settings_controller = SettingsController(main_window_ui)
-    fetch_reports_controller = FetchReportsController(manage_vendors_controller.vendors, settings_controller.settings,
-                                                      main_window_ui)
-    fetch_special_reports_controller = FetchSpecialReportsController(manage_vendors_controller.vendors,
-                                                                     settings_controller.settings, main_window_ui)
-    search_controller = SearchController(main_window_ui)
-    import_file_controller = ImportFileController(manage_vendors_controller.vendors, settings_controller.settings,
-                                                  main_window_ui)
-    visual_controller = VisualController(main_window_ui)
-    # endregion
-
-    # region Connect Signals
-    manage_vendors_controller.vendors_changed_signal.connect(fetch_reports_controller.on_vendors_changed)
-    manage_vendors_controller.vendors_changed_signal.connect(fetch_special_reports_controller.on_vendors_changed)
-    manage_vendors_controller.vendors_changed_signal.connect(import_file_controller.on_vendors_changed)
-    # endregion
+    # # region Setup Tab Controllers
+    # manage_vendors_controller = ManageVendorsController(main_window_ui)
+    # settings_controller = SettingsController(main_window_ui)
+    # fetch_reports_controller = FetchReportsController(manage_vendors_controller.vendors, settings_controller.settings,
+    #                                                   main_window_ui)
+    # fetch_special_reports_controller = FetchSpecialReportsController(manage_vendors_controller.vendors,
+    #                                                                  settings_controller.settings, main_window_ui)
+    # search_controller = SearchController(main_window_ui)
+    # import_file_controller = ImportFileController(manage_vendors_controller.vendors, settings_controller.settings,
+    #                                               main_window_ui)
+    # visual_controller = VisualController(main_window_ui)
+    # # endregion
+    #
+    # # region Connect Signals
+    # manage_vendors_controller.vendors_changed_signal.connect(fetch_reports_controller.on_vendors_changed)
+    # manage_vendors_controller.vendors_changed_signal.connect(fetch_special_reports_controller.on_vendors_changed)
+    # manage_vendors_controller.vendors_changed_signal.connect(import_file_controller.on_vendors_changed)
+    # # endregion
 
     main_window.show()
     sys.exit(app.exec_())
