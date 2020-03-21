29b448e0bdb13bbe1c81df7ede9c9144b2ff1ae9
sesan07 <31746240+sesan07@users.noreply.github.com>
2020-03-21 02:31:58 -0300

    Set the master document to index.rst for documentation

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,9 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# The master document
+master_doc = 'index'
+
 
 # -- Options for HTML output -------------------------------------------------
 
