20a30c3852a2c17168531457ca55cf6cfee195a1
zihhuang <43175319+zihhuang@users.noreply.github.com>
2020-04-13 19:02:39 -0300

    Add tip button
    Add help tip button at top of Date Range: "many vendors do not support a date range longer than 12 consecutive months"

--- FetchData.py
+++ FetchData.py
@@ -1438,6 +1438,10 @@ class FetchSpecialReportsController(FetchReportsAbstract):
         # endregion
 
         # region Date Edits
+        self.date_range_help_btn = fetch_special_reports_ui.date_range_help_button
+        self.date_range_help_btn.clicked.connect(
+            lambda: GeneralUtils.show_message("Many vendors do not support a date range longer than 12 consecutive months."))
+
         self.begin_date_edit_year = fetch_special_reports_ui.begin_date_edit_special_year
         self.begin_date_edit_year.setDate(self.begin_date)
         self.begin_date_edit_year.dateChanged.connect(lambda date: self.on_date_year_changed(date, "begin_date"))
